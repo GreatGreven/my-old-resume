0dd6fca97a7a31f23955aa21789720b75d44b6e5
Eric Grevillius <ericgrevillius@gmail.com>
2020-04-27 16:04:08 +0200

    Updates

---
+++ precache-manifest.5383d4a186ed7980e04e2291c38da63a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb794e606c1f02849860d7e77ff676e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76d4356f6d86b5c9c5ba",
+    "url": "/static/css/main.88f4dcd5.chunk.css"
+  },
+  {
+    "revision": "857fecc4ae500fecaf44",
+    "url": "/static/js/2.16d3f277.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.16d3f277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76d4356f6d86b5c9c5ba",
+    "url": "/static/js/main.7154d589.chunk.js"
+  },
+  {
+    "revision": "38f5eea42d65656df679",
+    "url": "/static/js/runtime-main.5409d948.js"
+  }
+]);
