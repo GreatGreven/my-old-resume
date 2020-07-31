b56b05487ad58697610b6bfda257c8c5bcef5500
GreatGreven <ericgrevillius@gmail.com>
2020-07-31 14:45:26 +0200

    added 404 to error page * animation and styling to ba added soon

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,7 +4,14 @@ import './NotFound.scss'
 export default function NotFound(){
     return (
         <div id="not-found">
-            <h1>Sorry, I haven't implemented this page.</h1>
+	    <h1>
+	        <span className='sliding-header'>4</span>
+	    <span className='sliding-header'>0</span>
+	    <span className='sliding-header'>4</span>
+	    </h1>
+            <h3>
+	        Sorry, I haven't implemented this page.
+	    </h3>
         </div>
     );
 }
